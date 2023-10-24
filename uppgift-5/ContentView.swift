5d2c854c7dc18790f3cc83aceb6d3511bc785723
Hendrik Ruijter <hendrik.ruijter@gmail.com>
2023-10-24 09:40:23 +0200

    Uppgift 5 baseline with specification.

--- uppgift-5/ContentView.swift
+++ uppgift-5/ContentView.swift
@@ -5,6 +5,13 @@
 //  Created by Hendrik on 2023-10-24.
 //
 
+/**
+ Skapa nytt projekt i Xcode.
+ Skapa egen modell i CreateML för image classify enligt video och länk. Kan använda exempelbilder eller själv plocka ner bilder för träning.
+ Använd kod från förra veckan för att använda modell i ett projekt. Lägg in helt annan bild i projeket av samma typ som du tränat på. Gör så vid tryck på knapp så skriver den ut på resultat på text på skärmen.
+ Lägg up projektet publikt på github och klista in länk nedan.
+ */
+
 import SwiftUI
 
 struct ContentView: View {
