81e300eb662e19f3207383a4e7e514a32b750aaf
Hendrik Ruijter <hendrik.ruijter@gmail.com>
2023-10-24 09:23:55 +0200

    Initial Commit

---
+++ uppgift-5/uppgift_5App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift_5App.swift
+//  uppgift-5
+//
+//  Created by Hendrik on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct uppgift_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
